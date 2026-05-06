ec716c1750a3dee842d25299830fa99325f24e56
Edvard Regner Cedell <Edvard@cedell.com>
2026-05-06 09:09:57 +0200

    GODTJÄN EJ!!!!

--- src/main/java/controller/DemoApplication.java
+++ src/main/java/controller/DemoApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	//för att testa actions
+	asdgsdga<sdga
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
